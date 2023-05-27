a7c8140519da9d6e2b29695102591df4d110998f
Paulius <jurgelis.paulius@gmail.com>
2023-05-27 15:14:16 +0300

    Add project files.

---
+++ Order_App/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order_App
+{
+     class Menu
+    {
+        private OrderManager orderManager;
+
+        public Menu()
+        {
+            orderManager = new OrderManager();
+        }
+
+        public void RunMenuUI()
+        {
+            bool exitApp = false;
+
+            while (exitApp == false)
+            {
+
+
+
+            }
+
+
+
+
+
+        }
+
+    }
+}
---
+++ Order_App/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Globalization;
+using Order_App;
+
+namespace Order_app
+{
+    class Entry
+    {
+        static string connectionString = @"Data Source=Orders.db";
+        static void Main(string[] args)
+        {
+            using (var connection = new SqliteConnection(connectionString)) 
+            {
+                connection.Open();
+
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText =
+                    @"CREATE TABLE IF NOT EXISTS order_table (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Date Text,
+                        Quantity INTEGER,
+                        Price INTEGER)";
+
+                tableCmd.ExecuteNonQuery();
+
+                connection.Close();
+            }
+
+            Menu menu = new Menu();
+            menu.RunMenuUI();
+        }
+    }
+}
